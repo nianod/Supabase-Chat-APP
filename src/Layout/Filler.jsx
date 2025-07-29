8013ac2e0285ce898e0970ef0e93f3f3c06df7d7
nianod <arnoldkk422@gmail.com>
2025-07-29 23:48:45 +0300

    added auth

--- src/Layout/Filler.jsx
+++ src/Layout/Filler.jsx
@@ -4,7 +4,7 @@ import { supabase } from '../Integration/Authcontext'
 
 const Filler = () => {
     const [message, setMessage] = useState('')
-    const [session, setSession] = useState([])
+    const [session, setSession] = useState(null)
 
     const send = (e) => {
         e.preventDefault()
@@ -30,32 +30,35 @@ const Filler = () => {
     const signOut = async () => {
       const { error } = await supabase.auth.signOut()
     }
+    console.log(session)
 
     if(!session) {
       return (
         <div className='flex flex-col items-center justify-center mt-12'>
           <h2 className='text-white font-semibold'> Sign in with Google</h2>
-          <button className='mt-2 bg-black text-white p-2 px-3  rounded h-full cursor-pointer'>Sign In</button>
+          <button onClick={signIn} className='mt-2 bg-black text-white p-2 px-3  rounded h-full cursor-pointer'>Sign In</button>
         </div>
       )
-    }
-
+    } else
   return (
     <div className='pb-20 p-10'>
       <h1 className='text-fuchsia-500 font-bold underline text-2xl text-center mt-5'>Connect with Your Loved ones</h1>
       <div className='border w-full h-screen min-h-[600px] border-white rounded mt-5'>
         <div className='flex justify-between border-b-1 border-gray-300 p-3'>
           <div className='text-gray-400'>
-            <p>Signed in as:</p>
+            <p>Signed in as: {session?.user?.user_metadata?.full_name}</p>
             <span>2 users online</span>
           </div>
           <div>
             <button onClick={signOut} className='bg-black text-white p-2 px-3  rounded h-full cursor-pointer'>sign Out</button>
           </div>
         </div>
+        <div className='overflow-y-auto min-h-[450px]'>
+
+        </div>
         <div className='mt-3 p-3'>
             <form className='gap-5 flex flex-col sm:flex-row'>
-                <input type="textarea"
+                <input type="text"
                    className='p-2 w-full bg-[#363535] rounded text-white'
                    placeholder='Type your message...'
                    value={message}
