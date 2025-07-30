748485bd83b4ab5b099334b16b16cc863b7fa294
nianod <arnoldkk422@gmail.com>
2025-07-30 20:13:33 +0300

    static users

--- src/Layout/Filler.jsx
+++ src/Layout/Filler.jsx
@@ -3,8 +3,10 @@ import { FaPaperPlane } from 'react-icons/fa'
 import { supabase } from '../Integration/Authcontext'
 
 const Filler = () => {
-    const [message, setMessage] = useState('')
+    const [newMessage, setNewMessage] = useState('')
     const [session, setSession] = useState(null)
+    const [usersOnline, setUsersOnline] = useState('')
+    const [messages, setNewMessages] = useState('') <div className="git commit -m ""></div>
 
     const send = (e) => {
         e.preventDefault()
@@ -46,7 +48,7 @@ const Filler = () => {
       <div className='border w-full h-screen min-h-[600px] border-white rounded mt-5'>
         <div className='flex justify-between border-b-1 border-gray-300 p-3'>
           <div className='text-gray-400'>
-            <p>Signed in as: {session?.user?.user_metadata?.full_name}</p>
+            <p>Signed in as: {session?.user?.email}</p>
             <span>2 users online</span>
           </div>
           <div>
@@ -56,13 +58,13 @@ const Filler = () => {
         <div className='overflow-y-auto min-h-[450px]'>
 
         </div>
-        <div className='mt-3 p-3'>
+        <div className='mt-3 p-3 border-t-[1.5px] border-gray-700'>
             <form className='gap-5 flex flex-col sm:flex-row'>
                 <input type="text"
                    className='p-2 w-full bg-[#363535] rounded text-white'
                    placeholder='Type your message...'
-                   value={message}
-                   onChange={(e) => setMessage(e.target.value)}
+                   value={newMessage}
+                   onChange={(e) => setNewMessage(e.target.value)}
                 />
                 <button
                   className='text-white flex gap-1 items-center bg-black p-2  rounded h-full cursor-pointer'
